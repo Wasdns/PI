a7b469a8cedbf2b497b5e7f1eca6fc2369bd7965
Wasdns <wasdnsxchen@gmail.com>
2018-02-24 08:56:44 -0800

    Add hands-on demo: l2_forward

---
+++ examples/l2_forward/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source $THIS_DIR/../env.sh
+
+P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
+
+SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
+
+CLI_PATH=$THIS_DIR/simple_switch_CLI
+
+$P4C_BM_SCRIPT p4src/switch.p4 --json switch.json
+sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python script/topo.py \
+    --behavioral-exe $SWITCH_PATH \
+    --json switch.json \
+    --cli $CLI_PATH \
+    --mode l3 
